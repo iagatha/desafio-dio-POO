c72c5bd2b3dfddeb8c28fff5a388236106e27ae8
iagatha <iagathasouzaa@gmail.com>
2022-11-16 15:37:19 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -22,8 +20,43 @@ public class Main {
         mentoria.setData(LocalDate.now());
 
 
-        System.out.println(mentoria);
+        /*System.out.println(mentoria);
         System.out.println(curso1);
-        System.out.println(curso2);
+        System.out.println(curso2);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Dev");
+        bootcamp.setDescricao("Descricao Bootcamp Java Dev");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+
+        Dev devIagatha = new Dev();
+        devIagatha.setNome("iagatha");
+        devIagatha.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Incritos" + devIagatha.getConteudosInscritos());
+        devIagatha.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Incritos" + devIagatha.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos" + devIagatha.getConteudosConcluidos());
+        System.out.println("XP: " + devIagatha.calcularTotalXp());
+
+
+
+        Dev devAfrodite = new Dev();
+        devAfrodite.setNome("Afrodite");
+        devAfrodite.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Incritos" + devIagatha.getConteudosInscritos());
+        devAfrodite.progredir();
+        devAfrodite.progredir();
+        devAfrodite.progredir();
+        System.out.println("-");
+        System.out.println("Conteudos Inscrito " + devAfrodite.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos " + devAfrodite.getConteudosConcluidos());
+        System.out.println("XP: " + devAfrodite.calcularTotalXp());
+
+
+
     }
 }
